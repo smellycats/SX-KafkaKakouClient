5ffe69d846b4cec7f98b064b11000cc2175e6706
SmellyCat <smellycat2014@foxmail.com>
2017-10-11 22:18:31 +0800

    添加 基础文件

---
+++ helper_kafka.py
@@ -0,0 +1,39 @@
+import time
+
+from my_logger import *
+from confluent_kafka import Producer, KafkaError, KafkaException, libversion
+
+
+logger = logging.getLogger('root')
+
+def error_cb(err):
+    #print('error_cb', err)
+    logger.error(err)
+
+
+class KafkaData(object):
+    def __init__(self, **kwargs):
+        try:
+            p = Producer()
+        except TypeError as e:
+            assert str(e) == "expected configuration dict"
+        self.p = Producer({'bootstrap.servers': '{0}'.format(kwargs['services']),
+                           'socket.timeout.ms': 1000, 'error_cb': error_cb,
+                           'retries': 3, 'retry.backoff.ms': 2000, 'batch.num.messages': 400,
+                           'default.topic.config': {'message.timeout.ms': 1000, 'acks': 'all'}})
+        self.topic = kwargs['topic']
+        self.lost_msg = []
+
+    def acked(self, err, msg):
+        if err is not None:
+            self.lost_msg.append(msg)
+            print(msg.key(), err)
+            logger.error(int(msg.key()))
+            logger.error(err)
+
+    def produce_info(self, key='', value=''):
+        self.p.produce(self.topic, value, key, callback=self.acked)
+
+    def flush(self):
+        self.p.poll(0.1)
+        self.p.flush()
---
+++ upload_data.py
@@ -0,0 +1,163 @@
+﻿# -*- coding: utf-8 -*-
+import time
+import json
+import base64
+
+import arrow
+
+from helper_kakou_v2 import Kakou
+from helper_consul import ConsulAPI
+from my_yaml import MyYAML
+from my_logger import *
+from helper_kafka import KafkaData
+
+
+debug_logging('/home/logs/error.log')
+logger = logging.getLogger('root')
+
+
+class UploadData(object):
+    def __init__(self):
+        # 配置文件
+        self.my_ini = MyYAML('/home/my.yaml').get_ini()
+
+        # request方法类
+        self.kk = None
+        self.ka = KafkaData(**dict(self.my_ini['kafka']))
+        self.con = ConsulAPI(**dict(self.my_ini['consul']))
+	
+        # ID上传标记
+        self.id_flag = 0
+        self.step = self.my_ini['step']
+        self.kkdd = self.my_ini['kkdd']
+
+        self.uuid = None                    # session id
+        self.session_time = time.time()     # session生成时间戳
+        self.ttl = dict(self.my_ini['consul'])['ttl']              # 生存周期
+        self.lock_name = dict(self.my_ini['consul'])['lock_name']  # 锁名
+        self.lost_list = []                 # 未上传数据列表
+
+    def get_id(self):
+        # 获取上传id
+        val = self.con.get_id()[0]['Value']
+        self.id_flag = json.loads(base64.b64decode(val).decode())
+
+    def set_id(self, _id):
+        """设置ID"""
+        r = self.con.get_id()
+        if self.con.put_id(_id, r[0]['ModifyIndex']):
+            self.id_flag = _id
+        print(_id)
+
+    def get_lost(self):
+        # 获取未上传数据id列表
+        value = self.con.get_lost()[0]['Value']
+        self.lost_list = json.loads(base64.b64decode(value).decode())
+
+    def post_lost_data(self):
+        """未上传数据重传"""
+        if len(self.lost_list) == 0:
+            return
+        for i in self.lost_list:
+            self.ka.produce_info(key=str(i['id']), value=json.dumps(i))
+            print('lost={0}'.format(i['id']))
+        self.ka.flush()
+        self.lost_list = []
+        if len(self.ka.lost_msg) > 0:
+            for i in self.ka.lost_msg:
+                self.lost_list.append(json.loads(i.value()))
+            self.ka.lost_msg = []
+        self.con.put_lost(json.dumps(self.lost_list))
+
+    def post_info(self):
+        """上传数据"""
+        info = self.kk.get_kakou(self.id_flag+1, self.id_flag+self.step, 1, self.step)
+        # 如果查询数据为0
+        if info['total_count'] == 0:
+            time.sleep(1)
+            return 0
+
+        data = []
+        for i in info['items']:
+            if i['kkdd_id'] is None or i['kkdd_id'] == '':
+                i['kkdd_id'] = self.kkdd
+            if i['hphm'] is None or i['hphm'] == '':
+                i['hphm'] = '-'
+            data.append(i)
+            self.ka.produce_info(key=str(i['id']), value=json.dumps(i))
+        self.ka.flush()
+        if len(self.ka.lost_msg) > 0:
+            for i in self.ka.lost_msg:
+                self.lost_list.append(json.loads(i.value()))
+            self.ka.lost_msg = []
+            self.con.put_lost(json.dumps(self.lost_list))
+        # 设置最新ID
+        self.set_id(info['items'][-1]['id'])
+        return info['total_count']
+
+    def get_lock(self):
+        """获取锁"""
+        if self.uuid is None:
+            self.uuid = self.con.put_session(self.ttl, self.lock_name)['ID']
+            self.session_time = time.time()
+            # 获取上传id
+            self.get_id()
+            self.get_lost()
+        # 大于一定时间间隔则更新session
+        t = time.time() - self.session_time
+        if t > (self.ttl - 10):
+            self.con.renew_session(self.uuid)
+            self.session_time = time.time()
+        l = self.con.get_lock(self.uuid)
+        #print(self.uuid, l)
+        # session过期
+        if l == None:
+            self.uuid = None
+            return False
+        return l
+
+    def get_service(self, service):
+        """获取服务信息"""
+        s = self.con.get_service(service)
+        if len(s) == 0:
+            return None
+        h = self.con.get_health(service)
+        if len(h) == 0:
+            return None
+        service_status = {}
+        for i in h:
+            service_status[i['ServiceID']] = i['Status']
+        for i in s:
+            if service_status[i['ServiceID']] == 'passing':
+                return {'host': i['ServiceAddress'], 'port': i['ServicePort']}
+        return None
+
+    def main_loop(self):
+        while 1:
+            if not self.get_lock():
+                time.sleep(2)
+                continue
+            if self.kk is not None and self.kk.status:
+                try:
+                    self.post_lost_data()
+                    n = self.post_info()
+                    if n >= self.step:
+                        time.sleep(0.5)
+                    else:
+                        time.sleep(1)
+                except Exception as e:
+                    logger.exception(e)
+                    time.sleep(15)
+            else:
+                try:
+                    if self.kk is None or not self.kk.status:
+                        s = self.get_service('kakouBldh')
+                        if s is None:
+                            time.sleep(5)
+                            continue
+                        self.kk = Kakou(**{'host':s['host'], 'port':s['port']})
+                        self.kk.status = True
+                except Exception as e:
+                    logger.error(e)
+                    time.sleep(1)
+        
